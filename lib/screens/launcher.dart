6634d89896a31f7445f0c01f37228e08705a30df
MUCHIRA JUNIOR <muchirajunior022@gmail.com>
2022-03-24 12:50:01 +0300

    project setup :star:

---
+++ lib/screens/launcher.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Launcher extends StatefulWidget {
+  const Launcher({ Key? key }) : super(key: key);
+
+  @override
+  _LauncherState createState() => _LauncherState();
+}
+
+class _LauncherState extends State<Launcher> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
